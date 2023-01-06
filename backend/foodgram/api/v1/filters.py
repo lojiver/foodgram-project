19d196c1f89a101913f012edc578b2970a7a0e3e
Anna Gorbunova <gorbunovaann@gmail.com>
2023-01-06 17:16:39 +0400

    Some filters fix

--- backend/foodgram/api/v1/filters.py
+++ backend/foodgram/api/v1/filters.py
@@ -22,7 +22,7 @@ class RecipeFilter(filters.FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ('author', 'tags')
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
 
 class IngredientFilter(filters.FilterSet):
