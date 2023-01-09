05badc10d959f1602fbc95e1adb8f7d71ce744bd
Anna Gorbunova <gorbunovaann@gmail.com>
2023-01-09 21:34:19 +0400

    ppp

--- backend/foodgram/api/v1/serializers.py
+++ backend/foodgram/api/v1/serializers.py
@@ -74,7 +74,7 @@ class RecipeGetSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'tags', 'author',
             'ingredients', 'is_favorited', 'is_in_shopping_cart',
-            'name', 'image', 'text', 'cooking_time',
+            'name', 'image', 'text', 'cooking_time'
         )
 
     def get_is_favorited(self, obj):
